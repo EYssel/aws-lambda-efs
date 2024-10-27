17275fd1009264f38650ff166a0769e1cfea5fe1
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-10-27 06:36:30 +0200

    Revert "chore: project created with projen"
    This reverts commit 4afa5aab4b19ae47cce3497e3074fef9d118476e.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,9 @@
 import { awscdk } from 'projen';
-const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Estian Yssel',
-  authorAddress: '38258302+EYssel@users.noreply.github.com',
+const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.5.0',
   name: 'aws-lambda-efs',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/EYssel/aws-lambda-efs',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
