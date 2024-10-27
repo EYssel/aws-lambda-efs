4afa5aab4b19ae47cce3497e3074fef9d118476e
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-10-27 06:29:08 +0200

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,13 @@
 import { awscdk } from 'projen';
-const project = new awscdk.AwsCdkTypeScriptApp({
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Estian Yssel',
+  authorAddress: '38258302+EYssel@users.noreply.github.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.5.0',
   name: 'aws-lambda-efs',
   projenrcTs: true,
+  repositoryUrl: 'https://github.com/EYssel/aws-lambda-efs',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
